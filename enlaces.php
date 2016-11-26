cdf954a2e6ff4062100cbf4bbdf0821a172537d5
Luis Augusto Claudio Ponce <megabyte1507@gmail.com>
2016-11-26 15:39:39 -0500

    actualizacion enlaces

--- enlaces.php
+++ enlaces.php
@@ -3,22 +3,22 @@
 <!-- css -->
 
 
-<link rel="stylesheet" href="http://localhost/cdn/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 <!-- js -->
-<script src="http://localhost/cdn/bootstrap/js/jquery.min.js"></script>
-<script src="http://localhost/cdn/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <!-- Fin Bootstrap 3  -->
 
 
 <!-- Inicio FontAwesome -->
 
-<link rel="stylesheet" href="http://localhost/cdn/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <!-- Inicio Google Fonts -->
 
 
-<link rel="stylesheet" href="http://localhost/cdn/google-fonts/montserrat.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
 <style>
 body{
@@ -36,11 +36,11 @@ $(function () {
 </script>
 
 
-<link rel="stylesheet" href="http://localhost/cdn/DataTables/media/css/dataTables.bootstrap.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
-<script src="http://localhost/cdn/DataTables/media/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
 
-<script src="http://localhost/cdn/DataTables/media/js/dataTables.bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
 
 <script>
 $(document).ready(function() {
